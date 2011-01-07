b093006aed432ceffbfcc6027aa3765f29478d67
Jamie Wong <jamie.lf.wong@gmail.com>
2011-01-07 11:15:03 -0500

    Winner propogation working

--- tournament/models.py
+++ tournament/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+def has_changed(instance, field):
+    if not instance.pk:
+        return False
+    old_value = instance.__class__._default_manager.\
+             filter(pk=instance.pk).values(field).get()[field]
+    return not getattr(instance, field) == old_value
+
 class Matchup(models.Model):
     name        = models.CharField(max_length=30)
     winner      = models.ForeignKey('players.player',blank=True,null=True)
@@ -30,6 +37,39 @@ class Matchup(models.Model):
                                                         verbose_name='Player 2',
                                                         help_text='Only set for first round matches')
     
+    def participants(self):
+        ret = []
+        if self.player_1_id:
+            ret += [self.player_1]
+        if self.player_2_id:
+            ret += [self.player_2]
+        return ret
+
+    def participant_ids(self):
+        ret = []
+        if self.player_1_id:
+            ret += [self.player_1_id]
+        if self.player_2_id:
+            ret += [self.player_2_id]
+        return ret
+
+    def __getattr__(self,name):
+        if name == 'loser':
+            if self.winner_id:
+                if self.winner == self.player_1:
+                    return self.player_2
+                else:
+                    return self.player_1
+            else:
+                return None
+        elif name == 'loser_id':
+            if self.loser == None:
+                return self.loser.id
+            else:
+                return None
+        else:
+            super(Matchup,self).__getattr(name)
+
     def __str__(self):
         if self.player_1_id:
             player_1_name = self.player_1.name
@@ -42,6 +82,34 @@ class Matchup(models.Model):
             player_2_name = '???'
 
         return "%s: %s vs. %s" % (self.name,player_1_name,player_2_name)
+
+    def save(self):
+        super(Matchup,self).save()
+
+        if self.winner_id and self.winner_matchup_id:
+            winner_matchup = self.winner_matchup
+
+            # Winner already propogated
+            if winner_matchup.player_1_id == self.winner.id:
+                return
+            if winner_matchup.player_2_id == self.winner.id:
+                return
+
+            # Winner changed, remove the loser from the slots
+            if winner_matchup.player_1_id == self.loser.id:
+                winner_matchup.player_1 = None
+            elif winner_matchup.player_2_id == self.loser.id:
+                winner_matchup.player_2 = None
+
+            # Propogate the winner
+            if winner_matchup.player_1_id == None:
+                winner_matchup.player_1 = self.winner
+            elif winner_matchup.player_2_id == None:
+                winner_matchup.player_2 = self.winner
+            else:
+                raise Exception('Cannot propogate winner')
+
+            winner_matchup.save()
 
     class Meta:
         ordering = ['name']
--- tournament/tests.py
+++ tournament/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from tournament.models import Matchup, Slot
+from tournament.models import Matchup
+from players.models import Player
 from django.test.client import Client
 from django.contrib.auth.models import User
 
@@ -22,3 +23,85 @@ class MatchupAdminTest(TestCase):
         resp = self.client.get('/admin/tournament/matchup/add/')
         self.assertEqual(resp.status_code,200)
         self.assertContains(resp,'Add matchup')
+
+    def test_get_list(self):
+        resp = self.client.get('/admin/tournament/matchup/')
+        self.assertEqual(resp.status_code,200)
+        self.assertContains(resp,'Select matchup to change')
+
+
+class MatchupTest(TestCase):
+    fixtures = ['players.json']
+
+    def test_method_participants(self):
+        alice   = Player.objects.get(name='alice')
+        bob     = Player.objects.get(name='bob')
+
+        matchup = Matchup(name='Test Matchup',player_1=alice,player_2=bob)
+        matchup.save()
+
+        self.assertTrue(alice in matchup.participants())
+        self.assertTrue(bob in matchup.participants())
+
+    def test_attr_loser(self):
+        alice   = Player.objects.get(name='alice')
+        bob     = Player.objects.get(name='bob')
+
+        matchup = Matchup(name='Test Matchup',player_1=alice,player_2=bob,winner=alice)
+        matchup.save()
+
+        self.assertEqual(matchup.winner,alice)
+        self.assertEqual(matchup.loser,bob)
+        
+
+    def test_winner_propogation(self):
+        alice   = Player.objects.get(name='alice')
+        bob     = Player.objects.get(name='bob')
+        candice = Player.objects.get(name='candice')
+        david   = Player.objects.get(name='david')
+
+        Matchup(name='Finals').save()
+
+        Matchup(name='Semi East',
+                player_1=alice,
+                player_2=bob,
+                winner_matchup=Matchup.objects.get(name='Finals')).save()
+
+        Matchup(name='Semi West',
+                player_1=candice,
+                player_2=david,
+                winner_matchup=Matchup.objects.get(name='Finals')).save()
+        
+        # Bob wins Semi East
+        semi_east = Matchup.objects.get(name='Semi East')
+        semi_east.winner = bob
+        semi_east.save()
+
+        # Candice wins Semi West
+        semi_west = Matchup.objects.get(name='Semi West')
+        semi_west.winner = candice
+        semi_west.save()
+
+        finals = Matchup.objects.get(name='Finals')
+        self.assertTrue(bob in finals.participants())
+        self.assertTrue(candice in finals.participants())
+
+        # Correction: Alice wins Semi East
+        semi_east = Matchup.objects.get(name='Semi East')
+        semi_east.winner = alice
+        semi_east.save()
+
+        finals = Matchup.objects.get(name='Finals')
+        self.assertTrue(alice in finals.participants())
+        self.assertTrue(bob not in finals.participants())
+        self.assertTrue(candice in finals.participants())
+
+        # Correction: David wins Semi West
+        semi_west = Matchup.objects.get(name='Semi West')
+        semi_west.winner = david
+        semi_west.save()
+
+        finals = Matchup.objects.get(name='Finals')
+        self.assertTrue(david in finals.participants())
+        self.assertTrue(candice not in finals.participants())
+        self.assertTrue(alice in finals.participants())
